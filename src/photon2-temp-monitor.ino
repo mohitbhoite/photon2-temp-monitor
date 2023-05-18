25729925fbb750ec9e397d1c2c5568efc04470de
Mohit Bhoite <mohitbhoite@users.noreply.github.com>
2023-05-18 16:05:07 -0700

    added comments to the code and added gitignore

--- src/photon2-temp-monitor.ino
+++ src/photon2-temp-monitor.ino
@@ -1,9 +1,3 @@
-/*
- * Project photon2-sh1107
- * Description:
- * Author:
- * Date:
- */
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 #include <Adafruit_SHT4x.h>
@@ -22,31 +16,31 @@ double battV = 0;
 unsigned long previousMillis = 0; 
 const long interval = 1000; //set how often you want to read and display sensor readings (in milliseconds)
 
-void setup() {
+void setup() 
+{
+  //Register the Particle variables that you can access over the cloud
+  Particle.variable("temp", tempC); //variable to hold temperature measurement
+  Particle.variable("humi", humi);  //variable to hold the humidity measurement
+  Particle.variable("batt", battV); //variable to hold the battery voltage measurement
 
-    Particle.variable("temp", tempC);
-    Particle.variable("humi", humi);
-    Particle.variable("batt", battV);
+  pinMode(S5, INPUT_PULLUP);
+  pinMode(D7, OUTPUT);
+  Serial.begin(115200);
 
-    pinMode(S5,INPUT_PULLUP);
-    pinMode(D7,OUTPUT);
-    Serial.begin(115200);
+  // Call function to initialize the sensor
+  initSHT40();
 
-    //Call function to initialize the sensor        
-    initSHT40();
+  // Initialize the OLED screen
+  display.begin(0x3C, true); // Address 0x3C default
+  display.clearDisplay();
+  display.display();
+  display.setRotation(1);
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE, SH110X_BLACK);
+  display.setCursor(0, 0);
 
-    //Initialize the OLED screen
-    display.begin(0x3C, true); // Address 0x3C default
-    display.clearDisplay();
-    display.display();
-    display.setRotation(1);
-    display.setTextSize(1);
-    display.setTextColor(SH110X_WHITE,SH110X_BLACK);
-    display.setCursor(0,0);
-
-    //Call function to draw the console on the OLED screen
-    drawConsole();
-
+  // Call function to draw the console on the OLED screen
+  drawConsole();
 }
 
 void loop() 
@@ -61,49 +55,52 @@ void loop()
   }
 }
 
-//Draw and update the console on the OLED display
+//Draw the console on the OLED screen
 void drawConsole(void)
 {
-    //Draw the console table
-    display.drawLine(42,0,42,44,SH110X_WHITE);
-    display.drawLine(84,0,84,44,SH110X_WHITE);
-    display.drawLine(0,14,128,14,SH110X_WHITE);
-    display.drawLine(0,44,128,44,SH110X_WHITE);
+  //Draw the console table
+  display.drawLine(42,0,42,44,SH110X_WHITE);
+  display.drawLine(84,0,84,44,SH110X_WHITE);
+  display.drawLine(0,14,128,14,SH110X_WHITE);
+  display.drawLine(0,44,128,44,SH110X_WHITE);
     
-    display.setCursor(10,4); display.print("Temp");
-    display.setCursor(54,4); display.print("Humi");
-    display.setCursor(96,4); display.print("Batt ");
-    display.display();
+  display.setCursor(10,4); display.print("Temp");
+  display.setCursor(54,4); display.print("Humi");
+  display.setCursor(96,4); display.print("Batt ");
+  display.display();
 
 }
 
 //Update the console with latest values
 void updateConsole(void)
 {
-    sensors_event_t humidity, temp;
-    sht4.getEvent(&humidity, &temp);
+  sensors_event_t humidity, temp;
+  sht4.getEvent(&humidity, &temp);
 
-    tempC = temp.temperature;
-    humi = humidity.relative_humidity;
-    battV = (analogRead(A6)*5.0)/4095;
+  tempC = temp.temperature;
+  humi = humidity.relative_humidity;
+  //LiPo positive terminal is connected to the A6 pin aka VBAT_MEAS
+  //This is a special 5V tolerant pin
+  //The ADC measurement is referenced to 5V instead of 3.3V
+  battV = (analogRead(A6)*5.0)/4095;//LiPo positive terminal is connected to the A6 pin
 
-    //Display a message on the bottom
-    //You can choose to use this space to display any text or information you want
-    display.setCursor(0,50); display.print("  Particle Photon 2");
+  //Display a message on the bottom
+  //You can choose to use this space to display any text or information you want
+  display.setCursor(0,50); display.print("  Particle Photon 2");
 
-    //Read and display the temperature
-    display.setCursor(6,20); display.print(tempC);
-    display.setCursor(20,32); display.print("C");
+  //Read and display the temperature
+  display.setCursor(6,20); display.print(tempC);
+  display.setCursor(20,32); display.print("C");
 
-    //Read and display the relative humidity
-    display.setCursor(50,20); display.print(humi);
-    display.setCursor(56,32); display.print("%rH");
+  //Read and display the relative humidity
+  display.setCursor(50,20); display.print(humi);
+  display.setCursor(56,32); display.print("%rH");
 
-    //Read and display the battery voltage
-    display.setCursor(92,20); display.print(battV);
-    if (digitalRead(S5)) display.print(" "); else display.print("*");
-    display.setCursor(102,32); display.print("V");
-    display.display();
+  //Read and display the battery voltage
+  display.setCursor(92,20); display.print(battV);
+  //if (digitalRead(S5)) display.print(" "); else display.print("*");
+  display.setCursor(102,32); display.print("V");
+  display.display();
 
 }
 
